a2b15ba8b02727ce2e121c85607257bc4f944c82
supriyada <supriyada@gmail.com>
2021-05-17 10:17:11 -0400

    Added starter code html and js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,52 @@
-// @TODO: YOUR CODE HERE!
+// The code for the chart is wrapped inside a function that
+// automatically resizes the chart
+function makeResponsive() {
+
+    // if the SVG area isn't empty when the browser loads,
+    // remove it and replace it with a resized version of the chart
+    var svgArea = d3.select("body").select("svg");
+  
+    // clear svg is not empty
+    if (!svgArea.empty()) {
+      svgArea.remove();
+    }
+  
+    // SVG wrapper dimensions are determined by the current width and
+    // height of the browser window.
+    var svgWidth = window.innerWidth;
+    var svgHeight = window.innerHeight-300;
+  
+    var margin = {
+      top: 50,
+      bottom: 50,
+      right: 50,
+      left: 50
+    };
+  
+    var height = svgHeight - margin.top - margin.bottom;
+    var width = svgWidth - margin.left - margin.right;
+  
+    // Append SVG element
+    var svg = d3
+      .select(".chart")
+      .append("svg")
+      .attr("height", svgHeight)
+      .attr("width", svgWidth);
+  
+    // Append group element
+    var chartGroup = svg.append("g")
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  
+    // Read CSV
+    d3.csv("./assets/data/data.csv").then(function(healthData) {
+        console.log(healthData)        
+    }).catch(function(error) {
+        console.log(error);
+    });
+}
+    
+// When the browser loads, makeResponsive() is called.
+makeResponsive();
+
+// When the browser window is resized, makeResponsive() is called.
+d3.select(window).on("resize", makeResponsive);
