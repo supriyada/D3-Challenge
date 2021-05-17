f3f747337b3fc0ab86468e654c96427a81110a36
supriyada <supriyada@gmail.com>
2021-05-17 11:16:02 -0400

    scatter plot 1

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -39,7 +39,79 @@ function makeResponsive() {
   
     // Read CSV
     d3.csv("./assets/data/data.csv").then(function(healthData) {
-        console.log(healthData)        
+        console.log(healthData)
+        
+        healthData.forEach(function(data) {
+            data.healthcare = +data.healthcare;
+            data.poverty = +data.poverty;
+            data.age = +data.age;
+            data.smokes = +data.smokes;
+            data.obesity = +data.obesity;
+            data.income = +data.income;
+          });
+
+        //Create scales
+        var xLinearScale1 = d3.scaleLinear()
+            .domain([d3.min(healthData, d => d.poverty)-1,d3.max(healthData, d => d.poverty)+2])
+            .range([0, width]);
+        var xLinearScale2 = d3.scaleLinear()
+            .domain(d3.extent(healthData, d => d.age))
+            .range([0, width]);
+        var xLinearScale3 = d3.scaleLinear()
+            .domain(d3.extent(healthData, d => d.income))
+            .range([0, width]);
+
+        var yLinearScale1 = d3.scaleLinear()
+            .domain([d3.min(healthData, d => d.healthcare)-1,d3.max(healthData, d => d.healthcare)+2])
+            .range([height, 0]);
+        var yLinearScale2 = d3.scaleLinear()
+            .domain(d3.extent(healthData, d => d.smokes))
+            .range([height, 0]);
+        var yLinearScale3 = d3.scaleLinear()
+            .domain(d3.extent(healthData, d => d.obesity))
+            .range([height, 0]);
+
+        //Create Axes
+        var x1Axis = d3.axisBottom(xLinearScale1);
+        var y1Axis = d3.axisLeft(yLinearScale1)//.ticks(6);
+
+        var x2Axis = d3.axisBottom(xLinearScale2);
+        var y2Axis = d3.axisLeft(yLinearScale2)//.ticks(6);
+
+        var x3Axis = d3.axisBottom(xLinearScale3);
+        var y3Axis = d3.axisLeft(yLinearScale3)//.ticks(6);
+        
+        //Append axes to the group
+        chartGroup.append("g")
+            .attr("transform", `translate(0, ${height})`)
+            .call(x1Axis);
+        chartGroup.append("g")
+            .call(y1Axis);
+
+        var circlesGroup = chartGroup.selectAll("circle")
+            .data(healthData)
+            .enter()
+            .append("circle")
+            .attr("cx",d => xLinearScale1(d.poverty))
+            .attr("cy",d => yLinearScale1(d.healthcare))
+            .attr("r", "10")
+            .attr("fill","red")
+            .attr("opacity", ".5")            
+
+        var text = chartGroup.selectAll(null)
+            .data(healthData)
+            .enter()
+            .append("text")
+            .attr("x", d => xLinearScale1(d.poverty))
+            .attr("y", d => yLinearScale1(d.healthcare)+3)
+            .attr("text-anchor", "middle")
+            .text(d=>d.abbr)
+            .attr("font-family", "sans-serif")
+            .attr("font-size", "10px")
+            .attr("fill", "black");
+        //    
+        //chartGroup.append("")
+
     }).catch(function(error) {
         console.log(error);
     });
